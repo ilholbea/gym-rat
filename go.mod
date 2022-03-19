92284a64eb3debd00e21c780e4e5aee785b6b6ff
Ionut-Laurentiu Holbea <ilholbea@gmail.com>
2022-03-19 13:52:12 +0100

    added go mod

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/ilholbea/gym-rat
+
+go 1.18
+
+require (
+	github.com/go-chi/chi/v5 v5.0.7
+	github.com/lib/pq v1.10.4
+)
--- internal/database.go
+++ internal/database.go
@@ -4,19 +4,19 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/lib/pq"
-
 )
 
 const (
 	host     = "127.0.0.1"
 	port     = "5432"
 	user     = "postgres"
-	password = "Microx23!"
+	password = "-"
 	dbname   = "gymRat"
 )
 
-type postgreDB struct
+type postgreDB struct {
 }
+
 func (pg *postgreDB) connectToDB() (*sql.DB, error) {
 	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 	db, err := sql.Open("postgres", psqlInfo)
